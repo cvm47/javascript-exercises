35c213175163ddc612000d7cba665960aac6549b
cvm47 <shivamuniyal1810@gmail.com>
2025-09-01 20:23:57 +0530

    ...moreArgs

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...moreArgs) {
+    for (const arg of moreArgs) {
+        let index = arr.indexOf(arg)
+        while (index !== -1) {
+            arr.splice(index, 1)
+        }
+        return arr
+    }
 };
 
 // Do not edit below this line
