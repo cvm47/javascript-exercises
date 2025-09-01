3ac9bca45be7e9993912229773719471787d03a2
cvm47 <shivamuniyal1810@gmail.com>
2025-09-01 20:46:43 +0530

    arrElementindex === -1 when absence of element in arr

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,12 +1,13 @@
 const removeFromArray = function(arr, ...moreArgs) {
     for (const arg of moreArgs) {
-        let index = arr.indexOf(arg)
-        while (index !== -1) {
+        while (arr.indexOf(arg) !== -1) {
+            let index = arr.indexOf(arg)
             arr.splice(index, 1)
         }
-        return arr
     }
+    return arr
 };
+// console.log(removeFromArray([1,2,2,3,4], 2, 4))
 
 // Do not edit below this line
 module.exports = removeFromArray;
