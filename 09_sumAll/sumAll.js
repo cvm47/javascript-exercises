358277180d2c79ceff723486e0901a2b4bd8775d
cvm47 <shivamuniyal1810@gmail.com>
2025-09-02 09:57:33 +0530

    Math.min() Math.max()

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
-};
+const sumAll = function (a, b) {
+    let sum = 0
+    for(let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        sum += i
+    }
+    return sum
+}
+console.log(1,4)
 
 // Do not edit below this line
 module.exports = sumAll;
