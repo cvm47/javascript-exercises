461e5b0b1a795407f03493879b0925e41ad69cfa
cvm47 <shivamuniyal1810@gmail.com>
2025-09-02 10:17:21 +0530

    clean code

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,8 +1,7 @@
 const sumAll = function (a, b) {
-    if ((a < 0 || b < 0)
-        || !(Number.isInteger(a) && Number.isInteger(b))) {
-        return 'ERROR'
-    }
+    if (a < 0 || b < 0) return 'ERROR'
+    if (!(Number.isInteger(a) && Number.isInteger(b))) return 'ERROR'
+
     let sum = 0
     for(let i = Math.min(a, b); i <= Math.max(a, b); i++) {
         sum += i
