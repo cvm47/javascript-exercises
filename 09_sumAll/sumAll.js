daeacbb48cb8e482e1939892e4865ddeddce452e
cvm47 <shivamuniyal1810@gmail.com>
2025-09-02 10:11:48 +0530

    only integers datatype allowed

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,6 @@
 const sumAll = function (a, b) {
-    if ((a < 0 || b < 0)) {
+    if ((a < 0 || b < 0)
+        || !(Number.isInteger(a) && Number.isInteger(b))) {
         return 'ERROR'
     }
     let sum = 0
