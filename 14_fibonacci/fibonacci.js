d92b686ce0a09d9d7ab434c06a468177c25bcc41
cvm47 <shivamuniyal1810@gmail.com>
2025-09-20 21:02:02 +0530

    palindrome bkl

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    
+    if (+num === 0) return 0
+    else if (+num === 1) return 1
+    else if (+num < 0) return 'OOPS'
+    else {
+        let a = 0, b = 1, c = a + b;
+        for (let i = 2; i < num; i++) {
+            a = b
+            b = c
+            c = a + b
+        }
+        return c
+    }
 };
 
 // Do not edit below this line
