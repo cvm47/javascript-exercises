25be20d08f41e778ab3ca433e62f49c3df0028e1
cvm47 <shivamuniyal1810@gmail.com>
2025-09-21 13:35:13 +0530

    const date = new Date()

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let ageArray = array.map((item) => {
+        if (item.yearOfDeath === undefined) {
+            const d = new Date()
+            item.yearOfDeath = d.getFullYear()
+        }
+        return item.yearOfDeath - item.yearOfBirth
+    })
+    let maxAge = Math.max(...ageArray)
+    let maxAgeIndex = ageArray.indexOf(maxAge)
+    return array[maxAgeIndex]
 };
 
 // Do not edit below this line
